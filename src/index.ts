14dfbbef6e755c7ab73b15238dc5c8fec0c2eee6
Hena Patel <hena.patel0002@temple.edu>
2025-09-23 20:43:10 -0400

    AAC 31: update 1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+export { commandHistory } from "./commandHistory";
+export { showHistoryPopup } from "./showHistoryPopup";
+export type { Command } from "./commandHistory";
+
+
 export function add(a: number, b: number): number {
   return a + b;
 }
+
+
+
+
