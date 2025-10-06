cc4d47cbec35ed9994ac9116c7c0df63876961d9
jesshutchison <tuq69738@temple.edu>
2025-10-06 12:39:29 -0400

    add command library class

---
+++ src/commandLibrary.ts
@@ -0,0 +1,39 @@
+/*Object representing game command with its associated properties.*/
+interface GameCommand {
+    /*name of the command*/
+    name: string;
+    /*callback function to execute the command*/
+    function: () => void;
+    /*description of the command*/
+    description: string;
+    /*icon representing the command*/
+    icon: void;
+    /*whether the command is active or not*/
+    active: boolean;
+}
+
+/**
+ * CommandLibrary contains a HashMap that:
+ * Can be called by CommandMapper.
+ * Maps a String command to the corresponding GameCommand.
+*/
+
+export class CommandLibrary {
+    private commandMap: Map<string, GameCommand>;
+
+    private static instance: CommandLibrary;
+
+    constructor() {
+        this.commandMap = new Map<string, GameCommand>();
+    }
+    
+    /**
+     * @returns The singleton instance of CommandLibrary.
+     */
+    public static getInstance(): CommandLibrary {
+        if (!CommandLibrary.instance) {
+            CommandLibrary.instance = new CommandLibrary();
+        }
+        return CommandLibrary.instance;
+    }
+}
